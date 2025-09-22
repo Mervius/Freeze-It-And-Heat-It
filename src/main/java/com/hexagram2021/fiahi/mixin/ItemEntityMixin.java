04ba5d88a70a92448308628508ed7a5368ab2f17
Mervius <dhagemeier2001@gmail.com>
2025-09-22 17:30:57 -0500

    temporary solution for ticking item entities to visually change when freezing or rotting

--- src/main/java/com/hexagram2021/fiahi/mixin/ItemEntityMixin.java
+++ src/main/java/com/hexagram2021/fiahi/mixin/ItemEntityMixin.java
@@ -1,5 +1,7 @@
 package com.hexagram2021.fiahi.mixin;
 
+import com.hexagram2021.fiahi.common.handler.ItemStackFoodHandler;
+import net.minecraft.nbt.CompoundTag;
 import com.hexagram2021.fiahi.common.ForgeEventHandler;
 import com.hexagram2021.fiahi.register.FIAHICapabilities;
 import com.hexagram2021.fiahi.register.FIAHIItems;
@@ -20,7 +22,25 @@ public abstract class ItemEntityMixin {
 		ItemEntity current = (ItemEntity) (Object) this;
 		if(!current.level().isClientSide && ForgeEventHandler.isAvailableToTickFood()) {
 			current.getItem().getCapability(FIAHICapabilities.FOOD_CAPABILITY).ifPresent(c -> {
-				c.foodTick(c.getTemperature() + 2.0D * WorldHelper.getTemperatureAt(current.level(), current.getOnPos()), current.getItem().getItem());
+				Double temperature = c.getTemperature();
+				int old_temp = (int)(temperature / 5) * 5;
+				c.foodTick(temperature + 2.0D * WorldHelper.getTemperatureAt(current.level(), current.getOnPos()), current.getItem().getItem());
+				temperature = c.getTemperature();
+				int new_temp = (int)(temperature / 5) * 5;
+				if (new_temp != old_temp) {
+					CompoundTag nbt = current.getItem().getTag();
+					if(nbt == null) {
+						if(temperature == 0) {
+							return;
+						}
+						nbt = new CompoundTag();
+					}
+					nbt.putInt(ItemStackFoodHandler.FIAHI_TAG_TEMPERATURE, new_temp);
+					ItemStack new_stack = new ItemStack(current.getItem().getItem(),current.getItem().getCount());
+					new_stack.setTag(nbt);
+					current.setItem(new_stack);
+					current.getItem().getCapability(FIAHICapabilities.FOOD_CAPABILITY).ifPresent(d -> {d.setTemperature(temperature);});
+				}
 				if(c.getTemperature() > 120) {
 					FoodProperties foodProperties = current.getItem().getItem().getFoodProperties();
 					if(foodProperties != null) {
